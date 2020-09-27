e6aec79f09f36cc5cabd1d98421d203dbe9956c7
Shelby Spees <shelbyspees@gmail.com>
2020-09-26 18:22:26 -0700

    Add validations for username and email

--- models/User.js
+++ models/User.js
@@ -1,8 +1,20 @@
 var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-  username: String,
-  email: String,
+  username: {
+    type: String,
+    lowercase: true,
+    require: [true, "can't be blank"],
+    match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+    index: true
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    required: [true, "can't be blank"],
+    match: [/\S+@\S+\.\S+/, 'is invalid'],
+    index: true
+  },
   bio: String,
   image: String,
   hash: String,
